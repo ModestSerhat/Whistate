74d40dd62b9587c4d1e6895a39a0a20a015710ff
ModestSerhat <mdstsrht@outlook.com>
2024-05-17 19:13:42 +0300

    Initial Commit

---
+++ Whistate/WhistateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhistateApp.swift
+//  Whistate
+//
+//  Created by Serhat İşbakan on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhistateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
