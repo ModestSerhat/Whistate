0cd98296a39bac6604fba5da67c25a691a708d23
Serhat İşbakan <mdstsrht@outlook.com>
2024-07-24 19:50:31 +0300

    Edited main page UI and circular download status view

--- Whistate/MainView.swift
+++ Whistate/MainView.swift
@@ -1,6 +1,7 @@
 // Code edited from https://gist.github.com/bfolkens/d5749c28830f02aadbf688ebe84fa6b7
 
 import SwiftUI
+import AVFoundation
 
 struct MainView: View {
     @Environment(\.colorScheme) var colorScheme
@@ -16,17 +17,13 @@ struct MainView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                ZStack(alignment: .topLeading) {
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(colorScheme == .dark ? .white : .gray, lineWidth: 2)
-                    ScrollView {
-                        Text(audioRecorder.transcribing ? "Transcribing..." : (audioRecorder.transcriptionText ?? "Record an audio to have it transcribed!"))
-                    }.padding(9)
-                }.frame(height: geometry.size.height * 8/10)
-                Spacer()
+                ScrollView {
+                    Text(audioRecorder.transcribing ? "Transcribing..." : (audioRecorder.transcriptionText ?? "Record an audio to have it transcribed!"))
+                }.frame(height: geometry.size.height * 8/10, alignment: .leading)
+                Divider()
                 HStack {
                     Toggle(isOn: $audioPlaying) {
-                        Image(systemName: audioPlaying ? "stop.fill" : "play.fill").resizable().frame(width: geometry.size.height/12.5, height: geometry.size.height/12.5)
+                        Image(systemName: audioPlaying ? "stop.fill" : "play.fill").resizable().frame(width: geometry.size.height/15, height: geometry.size.height/15)
                     }
                     .toggleStyle(.button)
                     .labelStyle(.iconOnly)
@@ -37,7 +34,8 @@ struct MainView: View {
                         } else {
                             audioPlayer.stopAudio()
                         }
-                    }.frame(width: geometry.size.height/10, height: geometry.size.height/10)
+                    }.frame(width: geometry.size.height/12.5, height: geometry.size.height/12.5)
+                    Spacer().frame(width: geometry.size.height/18.75)
                     GeometryReader { recordButtonGeometry in
                         ZStack {
                             Circle()
@@ -46,18 +44,20 @@ struct MainView: View {
                                 .animation(.easeInOut(duration: 1), value: audioRecorder.recording)
                                 .foregroundColor(.red)
                         }
-                    }.frame(width: geometry.size.height/10, height: geometry.size.height/10)
-                    Button(action: {
-                        
-                    }, label: {
-                        ZStack {
-                            Text(String(format: "%.2f", audioRecorder.downloadProgress*100) + "%").frame(width: geometry.size.height/10, height: geometry.size.height/10)
-                            CircularProgressView(progress: audioRecorder.downloadProgress)
-                        }
-                        
-                    }).frame(width: geometry.size.height/10, height: geometry.size.height/10)
-                }
-                Spacer()
+                    }.frame(width: geometry.size.height/12.5, height: geometry.size.height/12.5)
+                    Spacer().frame(width: geometry.size.height/18.75)
+                    GeometryReader { downloadButtonGeometry in
+                        let fontSize = downloadButtonGeometry.size.height / 5
+                        Button(action: {
+                            
+                        }, label: {
+                            ZStack {
+                                audioRecorder.downloadCompleted ? Text("Done!").font(.system(size: fontSize)) : Text(String(format: "%.2f", audioRecorder.downloadProgress*100) + "%").font(.system(size: fontSize))
+                                CircularProgressView(progress: audioRecorder.downloadCompleted ? 1 : audioRecorder.downloadProgress)
+                            }
+                        }).frame(width: downloadButtonGeometry.size.height, height: downloadButtonGeometry.size.height)
+                    }.frame(width: geometry.size.height/12.5, height: geometry.size.height/12.5)
+                }.padding(15)
             }.padding(15)
         }
         .task {
